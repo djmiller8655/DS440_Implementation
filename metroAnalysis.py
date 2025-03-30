ca5379e2adbcd04c8c69c861ffd74893c1a1ed95
djmiller8655 <151090285+djmiller8655@users.noreply.github.com>
2025-03-30 16:00:57 -0400

    Update metroAnalysis.py

--- metroAnalysis.py
+++ metroAnalysis.py
@@ -49,30 +49,6 @@ class MetroAnalysisApp(tk.Tk):
         if cont == "MetroAnalysisEventHomeFrame":
             frame.createWidgets()
         frame.tkraise()
-
-    def generateQuery(self):
-        #sqlite3 Metro.db <<EOS
-        #.mode csv
-        #.import rawData.csv Zip
-        #EOS
-        '''
-        table = self.grouptingType.get()
-        start_date = self.reportStartDate.get()
-        end_date = self.reportEndDate.get()
-        #grouping_type = self.groupingType.get()
-        national_appreciation = self.nationalAppreciation.get()
-        estimate_length = self.estimateLength.get()
-
-        # Create the SELECT statement
-        query = f"""
-        SELECT * FROM '{table}'
-        WHERE report_date BETWEEN '{start_date}' AND '{end_date}'
-        AND national_appreciation = '{national_appreciation}'
-        AND estimate_length = '{estimate_length}'
-        """
-        return query
-        '''
-        pass
         
 
     def returnToMainMenu(self):
